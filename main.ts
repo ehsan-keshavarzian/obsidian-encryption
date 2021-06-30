4efd97e41f25ea93dad8f2e98b0d39c47332323d
ehsan-keshavarzian <ehsan985@gmail.com>
2021-06-30 19:03:38 +1000

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,26 @@
 import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import crypto from 'crypto';
+
+enum Algorithm {
+	aes256cbc = 'aes-256-cbc'	
+}
+
+enum Encoding {
+	hex = 'hex'	
+}
 
 interface ObsidianEncryptionSettings {
 	Password: string;
+	Algorithm: string;
+	Encoding: string;
+	IV_Length: int;
 }
 
 const DEFAULT_SETTINGS: ObsidianEncryptionSettings = {
-	Password: ''
+	Password: '',
+	Algorithm: 'aes-256-cbc',
+	Encoding: 'hex',
+	IV_Length: 16
 }
 
 export default class ObsidianEncryption extends Plugin {
@@ -17,11 +32,18 @@ export default class ObsidianEncryption extends Plugin {
 		await this.loadSettings();
 
 		this.addRibbonIcon('lock', 'Encrypt', () => {
-			
+			const iv = crypto.randomBytes(settings.IV_Length);
+			const cipher = crypto.createCipheriv(settings.Algorithm, new Buffer(settings.Password), iv);
+			const encryptedData = Buffer.concat([cipher.update(data,), cipher.final(), iv]).toString(settings.Encoding);
 		});
 
-    this.addRibbonIcon('lock-open', 'Decrypt', () => {
-			
+   		this.addRibbonIcon('lock-open', 'Decrypt', () => {
+			const binaryData = new Buffer(data, settings.Encoding);
+			const iv = binaryData.slice(-settings.IV_Length);
+			const encryptedData = binaryData.slice(0, binaryData.length - settings.IV_Length);
+			const decipher = crypto.createDecipheriv(settings.Algorithm, new Buffer(settings.Password), iv);
+
+			const decryptedData = Buffer.concat([decipher.update(encryptedData), decipher.final()]).toString();
 		});
 
 		this.addCommand({
